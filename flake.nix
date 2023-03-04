154cb6edc6595f417e84bd4a0eec99f77c7659b3
Sam Rose <sam@floxdev.com>
2023-03-04 16:48:42 -0500

    save

--- flake.nix
+++ flake.nix
@@ -50,6 +50,12 @@
 
         #   propagatedBuildInputs = [];
         # };
+        postBuild = ''
+        # for external task you need a workaround for the no deps check flag
+        # https://github.com/phoenixframework/phoenix/issues/2690
+        mix do deps.loadpaths --no-deps-check, phx.digest
+        mix phx.digest --no-deps-check
+        '';
       };
 
     devShells.x86_64-linux.default = let
